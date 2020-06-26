65b112d12d9bd37452d72cb3c10eb356a8ba73d4
Mayank <mayankjain123450@gmail.com>
2020-06-26 11:36:30 +0530

    JUnit test for testing employee

--- employee-system/src/test/java/com/spring/cloud/employeesystem/EmployeeSystemApplicationTests.java
+++ employee-system/src/test/java/com/spring/cloud/employeesystem/EmployeeSystemApplicationTests.java
@@ -1,13 +1,31 @@
 package com.spring.cloud.employeesystem;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spring.cloud.employeesystem.entity.Employee;
+import com.spring.cloud.employeesystem.repo.EmployeeRepository;
 
 @SpringBootTest
 class EmployeeSystemApplicationTests {
-
+	
+	@Autowired
+	private EmployeeRepository empRepo;
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Test
-	void contextLoads() {
+	@Transactional
+	void findPanByEmployeeId() {
+		Optional<Employee> employee = empRepo.findById(10001);
+		logger.info("Employee Details of Id 10001 -> {}", employee.get());
+		logger.info("Pan Details of employee 10001-> {}", employee.get().getPan());
 	}
 
 }
