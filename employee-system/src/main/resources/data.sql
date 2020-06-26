8bd04183d8b7ce6e7f1f706a482d5ff743694565
Mayank <mayankjain123450@gmail.com>
2020-06-26 11:35:29 +0530

    Change in Database for employee table

--- employee-system/src/main/resources/data.sql
+++ employee-system/src/main/resources/data.sql
@@ -16,13 +16,17 @@ insert into pan(pan_id, pan_number, address)
 	values (30003, 'AFMPA2796L', 'San Fransisco');
 insert into pan(pan_id, pan_number, address)
 	values (30004, 'DPNKS1916K', 'New Jersey');
+insert into pan(pan_id, pan_number, address)
+	values (30005, 'EQNKJ1811J', 'San Diago');
 
-insert into employee(emp_id, emp_name, dept_name, birth_date, pan_pan_id, company_comp_id)
+insert into employee(emp_id, emp_name, dept_name, birth_date, pan_id, comp_id)
 	values (10001, 'Clint Barton', 'Finance', '1981-04-27 00:00:00', 30001, 20001);
-insert into employee(emp_id, emp_name, dept_name, birth_date, pan_pan_id, company_comp_id)
+insert into employee(emp_id, emp_name, dept_name, birth_date, pan_id, comp_id)
 	values (10002, 'Steve Rogers', 'Research', '1989-01-08 00:00:00', 30002, 20002);	
-insert into employee(emp_id, emp_name, dept_name, birth_date, pan_pan_id, company_comp_id)
+insert into employee(emp_id, emp_name, dept_name, birth_date, pan_id, comp_id)
 	values (10004, 'Tony Stark', 'Finance', '1990-03-28 00:00:00', 30003, 20004);	
-insert into employee(emp_id, emp_name, dept_name, birth_date, pan_pan_id, company_comp_id)
+insert into employee(emp_id, emp_name, dept_name, birth_date, pan_id, comp_id)
 	values (10005, 'Peter Parker', 'Finance', '1992-06-18 00:00:00', 30004, 20004);	
+insert into employee(emp_id, emp_name, dept_name, birth_date, pan_id, comp_id)
+	values (10006, 'Carol Denvers', 'Engineering', '1989-05-11 00:00:00', 30005, 20004);	
 
